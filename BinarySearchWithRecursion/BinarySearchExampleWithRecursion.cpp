6c140447afb056f2a0694bf0800b1ee6a7fd30ae
NearlyHeadlessJack <56494680+NearlyHeadlessJack@users.noreply.github.com>
2021-03-11 12:26:00 +0900

    Update BinarySearchExampleWithRecursion.cpp
    fix bugs

--- BinarySearchWithRecursion/BinarySearchExampleWithRecursion.cpp
+++ BinarySearchWithRecursion/BinarySearchExampleWithRecursion.cpp
@@ -5,7 +5,9 @@ by N.H.J.
 */
 #include <iostream>
 
-int array[100];// Input array
+using namespace std;
+
+int tarray[100];// Input array
 int numOfNumbers=0;
 int target;
 int i,j,k=0;
@@ -17,62 +19,57 @@ int binarySearch(int target,int right,int left);
 
 int main()
 {
-	_Input();
-	int a=binarySearch(target,numOfArrays-1,0);
-	if(a==-1)
-		cout<<"No target found!"<endl;
-	else
-		cout<<target<<" is No."<<a<<endl;
+    _Input();
+    int a=binarySearch(target,numOfNumbers-1,0);
+    if(a==-1)
+        cout<<"No target found!"<<endl;
+    else
+        cout<<target<<" is No."<<a+1<<endl;
 
 }
 
 int _Input()
 {
-	cout<<"Please input the number of numbers:"<<endl;
-	cin>>numOfArrays;
-	cout<<"Please input the array:"<<endl;	
-	for(i=0;i<=numOfArrays-1;i++)
-	{
-		cin>>array[i];
-	}
+    cout<<"Please input the number of numbers:"<<endl;
+    cin>>numOfNumbers;
+    cout<<"Please input the array:"<<endl;
+    for(i=0;i<=numOfNumbers-1;i++)
+    {
+        cin>>tarray[i];
+    }
 
-	cout<<"Please input your target:"<<endl;
-	cin>>target;
+    cout<<"Please input your target:"<<endl;
+    cin>>target;
 
-	return 0;
+    return 0;
 
 }
 
 int binarySearch(int target,int right,int left)
 {
-	if(left==right)//adhoc()
-	{
-		if(array[left]==target)
-			return left;
-			
-		else
-			return -1;
-			
-	}
-	else
-	{
-		int mid=left+(right-left)/2;//To avoid overflow
-		if(array[mid]==target)
-			a=mid;
-		else if(array[mid]<target)
-		{
-			return binarySearch(target,right,mid+1);
-		}
-		else if(array[mid]>target)
-		{
-			return binarySearch(target,mid-1,left);
-		}
+    if(left==right)//adhoc()
+    {
+        if(tarray[left]==target)
+            return left;
+            
+        else
+            return -1;
+            
+    }
+    else
+    {
+        int mid=left+(right-left)/2;//To avoid overflow
+        if(tarray[mid]==target)
+            return mid;
+        else if(tarray[mid]<target)
+        {
+            return binarySearch(target,right,mid+1);
+        }
+        else if(tarray[mid]>target)
+        {
+            return binarySearch(target,mid-1,left);
+        }
 
-	}
-	
+    }
+    return -1;
 }
-
-
-
-
-
