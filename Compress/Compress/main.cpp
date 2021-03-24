3f1e71ff71aeb4dff75ca8595ae374377df7d035
XcodeOfNHJ <1217423985@qq.com>
2021-03-24 16:04:10 +0900

    Ver.1

---
+++ Compress/Compress/main.cpp
@@ -0,0 +1,53 @@
+/*
+Compress
+by N.H.J.
+2021.3.23
+*/
+#include<iostream>
+
+int n;
+int p[],s[],l[],b[];
+
+void Compress(void);
+
+
+
+int main()
+{
+
+
+
+    cout<<"Please input the number of pixels:"<<endl;
+    cin>>n;
+    for(int i;i<n;i++)
+    {
+        cout<<"Please input Gray value of No."<<i+1<<" pixel:"<<endl;
+        cin>>p[i];
+    }
+
+
+
+
+
+
+
+}
+
+
+void Compress(void)
+{
+
+    int Lmax=255,header=11;
+    s[0]=0;
+    
+
+
+
+
+
+
+
+
+
+
+}
